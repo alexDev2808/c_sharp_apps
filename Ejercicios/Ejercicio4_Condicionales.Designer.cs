c4aa83711548b271f76cfe36b612e4d46a050fc7
alexh2808 <colores.2808.8000@gmail.com>
2023-11-22 19:00:09 -0600

    Ejercicio 4 terminado

--- Ejercicios/Ejercicio4_Condicionales.Designer.cs
+++ Ejercicios/Ejercicio4_Condicionales.Designer.cs
@@ -39,19 +39,19 @@
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.radioButton4 = new System.Windows.Forms.RadioButton();
-            this.radioButton5 = new System.Windows.Forms.RadioButton();
-            this.radioButton6 = new System.Windows.Forms.RadioButton();
-            this.radioButton7 = new System.Windows.Forms.RadioButton();
-            this.radioButton8 = new System.Windows.Forms.RadioButton();
-            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.tbPreview = new System.Windows.Forms.TextBox();
+            this.rbLarge = new System.Windows.Forms.RadioButton();
+            this.rbMedium = new System.Windows.Forms.RadioButton();
+            this.rbSmall = new System.Windows.Forms.RadioButton();
+            this.rbRojo = new System.Windows.Forms.RadioButton();
+            this.rbNaranja = new System.Windows.Forms.RadioButton();
+            this.rbAmarillo = new System.Windows.Forms.RadioButton();
+            this.rbVerde = new System.Windows.Forms.RadioButton();
+            this.rbAzul = new System.Windows.Forms.RadioButton();
+            this.rbMorado = new System.Windows.Forms.RadioButton();
             this.panel1.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -148,12 +148,12 @@
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.radioButton9);
-            this.groupBox2.Controls.Add(this.radioButton8);
-            this.groupBox2.Controls.Add(this.radioButton7);
-            this.groupBox2.Controls.Add(this.radioButton6);
-            this.groupBox2.Controls.Add(this.radioButton5);
-            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Controls.Add(this.rbMorado);
+            this.groupBox2.Controls.Add(this.rbAzul);
+            this.groupBox2.Controls.Add(this.rbVerde);
+            this.groupBox2.Controls.Add(this.rbAmarillo);
+            this.groupBox2.Controls.Add(this.rbNaranja);
+            this.groupBox2.Controls.Add(this.rbRojo);
             this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(70)))), ((int)(((byte)(156)))));
@@ -166,9 +166,9 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.radioButton3);
-            this.groupBox1.Controls.Add(this.radioButton2);
-            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.rbSmall);
+            this.groupBox1.Controls.Add(this.rbMedium);
+            this.groupBox1.Controls.Add(this.rbLarge);
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(70)))), ((int)(((byte)(156)))));
@@ -181,26 +181,27 @@
             // 
             // panel3
             // 
-            this.panel3.Controls.Add(this.button1);
+            this.panel3.Controls.Add(this.btnAgregar);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel3.Location = new System.Drawing.Point(0, 364);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(596, 60);
             this.panel3.TabIndex = 2;
             // 
-            // button1
+            // btnAgregar
             // 
-            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(70)))), ((int)(((byte)(156)))));
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(249, 8);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(129, 42);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Agregar";
-            this.button1.UseVisualStyleBackColor = false;
+            this.btnAgregar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnAgregar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(70)))), ((int)(((byte)(156)))));
+            this.btnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.ForeColor = System.Drawing.Color.White;
+            this.btnAgregar.Location = new System.Drawing.Point(249, 8);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(129, 42);
+            this.btnAgregar.TabIndex = 0;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
             // 
             // panel2
             // 
@@ -216,7 +217,7 @@
             // 
             // panel4
             // 
-            this.panel4.Controls.Add(this.textBox1);
+            this.panel4.Controls.Add(this.tbPreview);
             this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel4.Location = new System.Drawing.Point(0, 424);
             this.panel4.Name = "panel4";
@@ -224,119 +225,120 @@
             this.panel4.Size = new System.Drawing.Size(596, 269);
             this.panel4.TabIndex = 3;
             // 
-            // textBox1
+            // tbPreview
             // 
-            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBox1.Location = new System.Drawing.Point(15, 0);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(566, 254);
-            this.textBox1.TabIndex = 0;
+            this.tbPreview.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbPreview.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPreview.Location = new System.Drawing.Point(15, 0);
+            this.tbPreview.Multiline = true;
+            this.tbPreview.Name = "tbPreview";
+            this.tbPreview.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbPreview.Size = new System.Drawing.Size(566, 254);
+            this.tbPreview.TabIndex = 0;
             // 
-            // radioButton1
+            // rbLarge
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(27, 51);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(107, 29);
-            this.radioButton1.TabIndex = 0;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "L ($190)";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.rbLarge.AutoSize = true;
+            this.rbLarge.Location = new System.Drawing.Point(27, 51);
+            this.rbLarge.Name = "rbLarge";
+            this.rbLarge.Size = new System.Drawing.Size(107, 29);
+            this.rbLarge.TabIndex = 0;
+            this.rbLarge.TabStop = true;
+            this.rbLarge.Text = "L ($190)";
+            this.rbLarge.UseVisualStyleBackColor = true;
             // 
-            // radioButton2
+            // rbMedium
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(27, 108);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(113, 29);
-            this.radioButton2.TabIndex = 1;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "M ($129)";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.rbMedium.AutoSize = true;
+            this.rbMedium.Location = new System.Drawing.Point(27, 108);
+            this.rbMedium.Name = "rbMedium";
+            this.rbMedium.Size = new System.Drawing.Size(113, 29);
+            this.rbMedium.TabIndex = 1;
+            this.rbMedium.TabStop = true;
+            this.rbMedium.Text = "M ($129)";
+            this.rbMedium.UseVisualStyleBackColor = true;
             // 
-            // radioButton3
+            // rbSmall
             // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(27, 166);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(99, 29);
-            this.radioButton3.TabIndex = 1;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "S ($99)";
-            this.radioButton3.UseVisualStyleBackColor = true;
+            this.rbSmall.AutoSize = true;
+            this.rbSmall.Location = new System.Drawing.Point(27, 166);
+            this.rbSmall.Name = "rbSmall";
+            this.rbSmall.Size = new System.Drawing.Size(99, 29);
+            this.rbSmall.TabIndex = 1;
+            this.rbSmall.TabStop = true;
+            this.rbSmall.Text = "S ($99)";
+            this.rbSmall.UseVisualStyleBackColor = true;
             // 
-            // radioButton4
+            // rbRojo
             // 
-            this.radioButton4.AutoSize = true;
-            this.radioButton4.ForeColor = System.Drawing.Color.Red;
-            this.radioButton4.Location = new System.Drawing.Point(28, 38);
-            this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(72, 29);
-            this.radioButton4.TabIndex = 1;
-            this.radioButton4.TabStop = true;
-            this.radioButton4.Text = "Rojo";
-            this.radioButton4.UseVisualStyleBackColor = true;
+            this.rbRojo.AutoSize = true;
+            this.rbRojo.ForeColor = System.Drawing.Color.Red;
+            this.rbRojo.Location = new System.Drawing.Point(28, 38);
+            this.rbRojo.Name = "rbRojo";
+            this.rbRojo.Size = new System.Drawing.Size(72, 29);
+            this.rbRojo.TabIndex = 1;
+            this.rbRojo.TabStop = true;
+            this.rbRojo.Text = "Rojo";
+            this.rbRojo.UseVisualStyleBackColor = true;
             // 
-            // radioButton5
+            // rbNaranja
             // 
-            this.radioButton5.AutoSize = true;
-            this.radioButton5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.radioButton5.Location = new System.Drawing.Point(28, 73);
-            this.radioButton5.Name = "radioButton5";
-            this.radioButton5.Size = new System.Drawing.Size(101, 29);
-            this.radioButton5.TabIndex = 1;
-            this.radioButton5.TabStop = true;
-            this.radioButton5.Text = "Naranja";
-            this.radioButton5.UseVisualStyleBackColor = true;
+            this.rbNaranja.AutoSize = true;
+            this.rbNaranja.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.rbNaranja.Location = new System.Drawing.Point(28, 73);
+            this.rbNaranja.Name = "rbNaranja";
+            this.rbNaranja.Size = new System.Drawing.Size(101, 29);
+            this.rbNaranja.TabIndex = 1;
+            this.rbNaranja.TabStop = true;
+            this.rbNaranja.Text = "Naranja";
+            this.rbNaranja.UseVisualStyleBackColor = true;
             // 
-            // radioButton6
+            // rbAmarillo
             // 
-            this.radioButton6.AutoSize = true;
-            this.radioButton6.ForeColor = System.Drawing.Color.Yellow;
-            this.radioButton6.Location = new System.Drawing.Point(28, 108);
-            this.radioButton6.Name = "radioButton6";
-            this.radioButton6.Size = new System.Drawing.Size(103, 29);
-            this.radioButton6.TabIndex = 1;
-            this.radioButton6.TabStop = true;
-            this.radioButton6.Text = "Amarillo";
-            this.radioButton6.UseVisualStyleBackColor = true;
+            this.rbAmarillo.AutoSize = true;
+            this.rbAmarillo.ForeColor = System.Drawing.Color.Yellow;
+            this.rbAmarillo.Location = new System.Drawing.Point(28, 108);
+            this.rbAmarillo.Name = "rbAmarillo";
+            this.rbAmarillo.Size = new System.Drawing.Size(103, 29);
+            this.rbAmarillo.TabIndex = 1;
+            this.rbAmarillo.TabStop = true;
+            this.rbAmarillo.Text = "Amarillo";
+            this.rbAmarillo.UseVisualStyleBackColor = true;
             // 
-            // radioButton7
+            // rbVerde
             // 
-            this.radioButton7.AutoSize = true;
-            this.radioButton7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.radioButton7.Location = new System.Drawing.Point(28, 143);
-            this.radioButton7.Name = "radioButton7";
-            this.radioButton7.Size = new System.Drawing.Size(86, 29);
-            this.radioButton7.TabIndex = 2;
-            this.radioButton7.TabStop = true;
-            this.radioButton7.Text = "Verde";
-            this.radioButton7.UseVisualStyleBackColor = true;
+            this.rbVerde.AutoSize = true;
+            this.rbVerde.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.rbVerde.Location = new System.Drawing.Point(28, 143);
+            this.rbVerde.Name = "rbVerde";
+            this.rbVerde.Size = new System.Drawing.Size(86, 29);
+            this.rbVerde.TabIndex = 2;
+            this.rbVerde.TabStop = true;
+            this.rbVerde.Text = "Verde";
+            this.rbVerde.UseVisualStyleBackColor = true;
             // 
-            // radioButton8
+            // rbAzul
             // 
-            this.radioButton8.AutoSize = true;
-            this.radioButton8.ForeColor = System.Drawing.Color.Blue;
-            this.radioButton8.Location = new System.Drawing.Point(28, 178);
-            this.radioButton8.Name = "radioButton8";
-            this.radioButton8.Size = new System.Drawing.Size(72, 29);
-            this.radioButton8.TabIndex = 3;
-            this.radioButton8.TabStop = true;
-            this.radioButton8.Text = "Azul";
-            this.radioButton8.UseVisualStyleBackColor = true;
+            this.rbAzul.AutoSize = true;
+            this.rbAzul.ForeColor = System.Drawing.Color.Blue;
+            this.rbAzul.Location = new System.Drawing.Point(28, 178);
+            this.rbAzul.Name = "rbAzul";
+            this.rbAzul.Size = new System.Drawing.Size(72, 29);
+            this.rbAzul.TabIndex = 3;
+            this.rbAzul.TabStop = true;
+            this.rbAzul.Text = "Azul";
+            this.rbAzul.UseVisualStyleBackColor = true;
             // 
-            // radioButton9
+            // rbMorado
             // 
-            this.radioButton9.AutoSize = true;
-            this.radioButton9.Location = new System.Drawing.Point(28, 213);
-            this.radioButton9.Name = "radioButton9";
-            this.radioButton9.Size = new System.Drawing.Size(100, 29);
-            this.radioButton9.TabIndex = 3;
-            this.radioButton9.TabStop = true;
-            this.radioButton9.Text = "Morado";
-            this.radioButton9.UseVisualStyleBackColor = true;
+            this.rbMorado.AutoSize = true;
+            this.rbMorado.Location = new System.Drawing.Point(28, 213);
+            this.rbMorado.Name = "rbMorado";
+            this.rbMorado.Size = new System.Drawing.Size(100, 29);
+            this.rbMorado.TabIndex = 3;
+            this.rbMorado.TabStop = true;
+            this.rbMorado.Text = "Morado";
+            this.rbMorado.UseVisualStyleBackColor = true;
             // 
             // Ejercicio4_Condicionales
             // 
@@ -375,20 +377,20 @@
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnAgregar;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.RadioButton radioButton3;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton6;
-        private System.Windows.Forms.RadioButton radioButton5;
-        private System.Windows.Forms.RadioButton radioButton4;
-        private System.Windows.Forms.RadioButton radioButton9;
-        private System.Windows.Forms.RadioButton radioButton8;
-        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.TextBox tbPreview;
+        private System.Windows.Forms.RadioButton rbSmall;
+        private System.Windows.Forms.RadioButton rbMedium;
+        private System.Windows.Forms.RadioButton rbLarge;
+        private System.Windows.Forms.RadioButton rbAmarillo;
+        private System.Windows.Forms.RadioButton rbNaranja;
+        private System.Windows.Forms.RadioButton rbRojo;
+        private System.Windows.Forms.RadioButton rbMorado;
+        private System.Windows.Forms.RadioButton rbAzul;
+        private System.Windows.Forms.RadioButton rbVerde;
     }
 }
--- Ejercicios/Ejercicio4_Condicionales.cs
+++ Ejercicios/Ejercicio4_Condicionales.cs
@@ -24,5 +24,50 @@ namespace CursoCSharp.Ejercicios
             lblTitulo4.Text = titulo;
         }
 
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Calcular();
+        }
+
+        private void Calcular()
+        {
+            int total = 0;
+            string detalle = "";
+
+            if (rbLarge.Checked)
+            {
+                total += 190;
+                detalle += "Talla: Large \r\n";
+            }
+            if (rbMedium.Checked)
+            {
+                total += 149;
+                detalle += "Talla: Medium \r\n";
+            }
+            if (rbSmall.Checked)
+            {
+                total += 99;
+                detalle += "Talla: Small \r\n";
+            }
+
+            if (rbRojo.Checked)
+            {
+                total += 10;
+                detalle += "Color: Rojo";
+            }
+            if (rbNaranja.Checked)
+            {
+                total += 15;
+                detalle += "Color: Naranja";
+            }
+            if (rbAzul.Checked)
+            {
+                total += 20;
+                detalle += "Color: Azul";
+            }
+            detalle += "\r\n\nEl total a pagar es $" + total + " MXN";
+            tbPreview.Text = detalle;
+
+        }
     }
 }
